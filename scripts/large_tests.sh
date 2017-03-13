f01528efea9a55e4fd21895ca62f034d9b44ca5f
Klaudiusz Dembler <k.dembler@gmail.com>
2017-03-13 17:13:59 +0100

    Updated LICENSE and NOTICE

--- scripts/large_tests.sh
+++ scripts/large_tests.sh
@@ -1,4 +1,21 @@
-#!/bin/bash  
+#!/bin/bash
+
+#http://www.apache.org/licenses/LICENSE-2.0.txt
+#
+#
+#Copyright 2016 Intel Corporation
+#
+#Licensed under the Apache License, Version 2.0 (the "License");
+#you may not use this file except in compliance with the License.
+#You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+#Unless required by applicable law or agreed to in writing, software
+#distributed under the License is distributed on an "AS IS" BASIS,
+#WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#See the License for the specific language governing permissions and
+#limitations under the License.
 
 set -e
 set -u
@@ -22,7 +39,7 @@ echo -n "[task is running] "
 task_list=$(snaptel task list | tail -1)
 if echo $task_list | grep -q Running; then
     echo "ok"
-else 
+else
     echo "not ok"
     return_code=-1
 fi
@@ -30,7 +47,7 @@ fi
 echo -n "[task is hitting] "
 if [ $(echo $task_list | awk '{print $4}') -gt 0 ]; then
     echo "ok"
-else 
+else
     _debug $task_list
     echo "not ok"
     return_code=-1
@@ -39,7 +56,7 @@ fi
 echo -n "[task has no errors] "
 if [ $(echo $task_list | awk '{print $6}') -eq 0 ]; then
     echo "ok"
-else 
+else
     echo "not ok"
     return_code=-1
 fi
